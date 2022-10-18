3f348a2e16dcc316cadad77ef9a25494389f02f8
Andrey Shumila <Andrii_Shumila@epam.com>
2022-10-18 19:25:27 +0300

    Components styles were added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Counter from "./components/counter/Counter";
+import Search from "./components/search/Search";
+import Filters from "./components/filters/Filters";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Counter/>
+            <hr/>
+            <Search/>
+            <hr/>
+            <Filters/>
+        </div>
+    );
 }
 
 export default App;
