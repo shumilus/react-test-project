e79e3c9d0ab4ba662bc4eadc396a753fdfbe3666
Andrey Shumila <Andrii_Shumila@epam.com>
2022-10-18 20:03:55 +0300

    1-module: Create component approuche was implemented

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React from "react";
 import Counter from "./components/counter/Counter";
 import Search from "./components/search/Search";
 import Filters from "./components/filters/Filters";
+import { CreateComponentTest } from "./components/Create-component-test";
 
 function App() {
     return (
         <div className="App">
+            <CreateComponentTest/>
+            <hr/>
             <Counter/>
             <hr/>
             <Search/>
